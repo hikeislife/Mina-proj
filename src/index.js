2f217e6ffea85c62f2babca5990f3fa8d4c22a97
hikeislife <hikeislife@users.noreply.github.com>
2019-05-20 12:47:13 +0200

    manifest edit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from 'react'; // object React from module react
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+const element = <h1>pozdrav svete</h1>;
+
+ReactDOM.render(element, document.querySelector("#root"));
+console.log(element);
 
 serviceWorker.register();
