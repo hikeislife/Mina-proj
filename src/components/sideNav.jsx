cd646cdfa0268fe4c0213d1e64db4b0acd4f9564
hikeislife <hikeislife@users.noreply.github.com>
2019-05-22 11:53:53 +0200

    top nav rework

--- src/components/sideNav.jsx
+++ src/components/sideNav.jsx
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import { getMenu } from '../data/menu';
+import TopNav from './topNav';
 
 class SideNav extends Component {
-	state = {
-		//current: this.props.id,
-		current: "js",
-		};
+	// state = {
+	// 	//current: this.TopNav.id,
+	// 	current: "js",
+	// 	};
 
 	renderSideMenu(current) {
-		let links = getMenu(this.state.current);
+		console.log(current)
+		let links = getMenu(current);
 		var menu = links.map(item => <a href={ item.urls } key={ item._id }><li key={item._id}>{ item.items }</li></a>)
 		return menu;
 	};
 
+
 	render() {
-		return ( <div className="sideNav">
+
+		//loadSideNav = id => {console.log("called from top nav", id);}
+
+		return ( <div className="sideNav" >
 			<ul>
-				{ this.renderSideMenu(this.state.current) }
+				{ this.renderSideMenu("mp") }
 			</ul>
 		</div> )
 	};
--- src/components/topNav.jsx
+++ src/components/topNav.jsx
@@ -4,25 +4,25 @@ import { getTopNav } from '../data/topNav';
 
 class TopNav extends Component {
 	state = {
-		//items: ["HTML & CSS", "CMD & Git", "JavaScript"],
-		//section: ["mp", "hc", "gc", "js"],
-		selection: this.props.id,
 		test: getTopNav(),
+		selection: "mp",
 		activeIndex: 0,
 	};
 
-	setActive = index => {
-		console.log(index)
+	setActive = selection => {
+		console.log(selection)
 		this.setState({
-			activeIndex: index !== this.state.activeIndex ? index : null
+			selection
 		});
-		//item.classList.add("active");
+		//SideNav.loadSideNav(selection);
+		//this.SideNav.renderSideMenu(selection);
+		
 	}
 
 	render() {
 		return ( <nav>
 			<ul>
-				{ this.state.test.map(t => <li key={t._id} id={t._id} onClick={() => this.setActive(1)} className={this.state.activeIndex === 0 ? "" : "active"} > { t.content } </li> ) }
+				{ this.state.test.map(t => <li key={t._id} id={t._id} onClick={() => this.setActive(t._id)} /*className={this.state.activeIndex === 0 ? "" : "active"}*/ > { t.content } </li> ) }
 			</ul>
 		</nav> )
 	}
