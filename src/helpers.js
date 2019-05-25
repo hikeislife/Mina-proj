2d7cc84d50b4a17d635e09636e358c87a4a08b6d
hikeislife <hikeislife@users.noreply.github.com>
2019-05-25 20:25:42 +0200

    resizer

--- src/helpers.js
+++ src/helpers.js
@@ -1,24 +1,15 @@
 // resizes text area containing code to fit the contents
 
-class Helpers {
-	state = {}
-
-
-
-// window.onload = () => {
-// 	textAreaSizer();
-// }
-	render() {
-	// 	return (  )
-
-textAreaSizer = () => {
+function textAreaSizer() {
  const text = document.getElementsByClassName("code_snippet");
  for (let i = 0; i < text.length; i ++) {
   let scroll = text[i].scrollHeight;
   text[i].style.height = text[i].scrollHeight + 'px';
  }
 }
-	 }
+
+window.onload = () => {
+	textAreaSizer();
 }
 
 export default Helpers;
