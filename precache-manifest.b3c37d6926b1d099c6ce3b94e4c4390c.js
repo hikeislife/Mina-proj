7ac9b0084adc0dd28da7821479e97d1be7b27d63
hikeislife <hikeislife@users.noreply.github.com>
2019-06-30 12:00:52 +0200

    Updates

---
+++ precache-manifest.b3c37d6926b1d099c6ce3b94e4c4390c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0249f2f74967d0ef11cc1eb1f47f7cb1",
+    "url": "/kodiranje-react/index.html"
+  },
+  {
+    "revision": "119d92f04e9608623341",
+    "url": "/kodiranje-react/static/css/2.ed0f3f40.chunk.css"
+  },
+  {
+    "revision": "6b0c20a40d718869439b",
+    "url": "/kodiranje-react/static/css/main.82d6464a.chunk.css"
+  },
+  {
+    "revision": "119d92f04e9608623341",
+    "url": "/kodiranje-react/static/js/2.a6bbc099.chunk.js"
+  },
+  {
+    "revision": "6b0c20a40d718869439b",
+    "url": "/kodiranje-react/static/js/main.37a9c895.chunk.js"
+  },
+  {
+    "revision": "4c216b6e6dc52a99f407",
+    "url": "/kodiranje-react/static/js/runtime~main.deb01543.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/kodiranje-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/kodiranje-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/kodiranje-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/kodiranje-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/kodiranje-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "69f9e5b1611d6b80b36ae3fb40de8fa8",
+    "url": "/kodiranje-react/static/media/logo.69f9e5b1.svg"
+  }
+]);
